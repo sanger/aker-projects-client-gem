29c0a7ee96dfacbf33d1e941c6a1fb8362003f83
David Robinson <dr6@sanger.ac.uk>
2017-03-03 09:32:38 +0000

    Added tests (spec) to study client

--- spec/study_client_spec.rb
+++ spec/study_client_spec.rb
@@ -4,4 +4,22 @@ describe StudyClient do
   it "has a version number" do
     expect(StudyClient::VERSION).not_to be nil
   end
+
+  it "has a cost_code" do
+    expect(StudyClient::Node.new).to respond_to :cost_code
+  end
+
+  it "is connected to the json api" do
+    url='http://localhost:9999/api/v1/'
+    id="123"
+    StudyClient::Base.site=url
+
+    stub_request(:get, url+"nodes/"+id).
+         with(headers: {'Accept'=>'application/vnd.api+json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/vnd.api+json', 'User-Agent'=>'Faraday v0.11.0'}).
+         to_return(status: 200, body: "", headers: {})
+    stub_request(:get, url+'/nodes')
+        .to_return(status: 200, body: {id: id}.to_json, headers: {})
+
+    expect(StudyClient::Node.find(id)).not_to be nil
+  end
 end
