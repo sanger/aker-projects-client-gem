565d87e784620aba32f0fee1c400ee105518cfd3
David Robinson <dr6@sanger.ac.uk>
2017-03-03 14:05:01 +0000

    Added StudyClient::Collection class so we can get the collection ids
    The url to get nodes with collections is
    api/v1/collections
    
    So the easiest way to get to it is with a class called Collection, even
    though it actually gets Nodes.

--- lib/study_client.rb
+++ lib/study_client.rb
@@ -2,13 +2,21 @@ require "study_client/version"
 require "json_api_client"
 
 module StudyClient
+
     class Base < JsonApiClient::Resource
         self.site = ENV['STUDY_URL']
     end
 
     class Node < Base
         def cost_code
-            self.send('cost-code')
+            attributes['cost-code']
+        end
+    end
+
+    class Collection < Node
+        # This is not really a collection. It is a node with a set-id.
+        def set_id
+            attributes['set-id']
         end
     end
 end
